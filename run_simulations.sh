74c04441d80b42396e8e90683a251fa907f80b69
foz <foz>
2024-05-31 09:11:40 +0100

    Fix runner scripts

--- run_simulations.sh
+++ run_simulations.sh
@@ -11,8 +11,6 @@ for pd in 0.1 0.08 0.06 0.04; do
     ./oink --replicas ${NS} --output_prefix ${p}/ --detect ${pd} --detect_after_first ${pd} --first_detect 0.0 &
 done
 wait
-done
-wait
 
 rm -r output2/
 mkdir output2/
