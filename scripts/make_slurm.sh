9cdde8df5679d86935639cb8ecee825602433c08
John Fozard <john.fozard@gmail.com>
2024-06-07 16:18:31 +0100

    Fix slurm makefile

--- scripts/make_slurm.sh
+++ scripts/make_slurm.sh
@@ -1,4 +1,4 @@
 module load libs/gcc/11.4.0
 module load libs/gsl/2.7/gcc-4.8.5
 export PATH=/opt/gridware/depots/996bcebb/el7/pkg/compilers/gcc/11.4.0/bin/:$PATH
-make -f Makefile_slurm PARALLEL=1
+make -f Makefile PARALLEL=1
